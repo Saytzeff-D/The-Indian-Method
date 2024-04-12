89c28b5d72cea60fe998b2eb7cad35ccde2bf23b
Felix Adegboyega <felixadegboyega2019@gmail.com>
2024-04-12 18:28:45 +0100

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+// import { useState } from "react"
 import logo from "../assets/logo.png"
 import { useLocation } from "react-router"
 import { navLinks } from "../utils/constants"
